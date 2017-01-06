6e26555add6d7f68936bc33d79a974cd5472562e
Alexander Kluchnik <spankyhamtony@gmail.com>
2017-01-06 10:07:29 +0200

    permission

--- app/src/main/java/com/kitsyambocka/wifitest/MainActivity.java
+++ app/src/main/java/com/kitsyambocka/wifitest/MainActivity.java
@@ -1,13 +1,20 @@
 package com.kitsyambocka.wifitest;
 
+import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -22,6 +29,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements OnListItemClick, OnConnectClick {
 
+    private static final int MY_PERMISSIONS_REQUEST = 1;
     private RecyclerView recyclerView;
     private Button button;
 
@@ -59,8 +67,6 @@ public class MainActivity extends AppCompatActivity implements OnListItemClick, 
             }
         }, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
 
-        wifi.startScan();
-
         results = new ArrayList<>();
 
         wifiListAdapter = new WifiListAdapter(results, this);
@@ -77,6 +83,13 @@ public class MainActivity extends AppCompatActivity implements OnListItemClick, 
 
             }
         });
+
+        if (checkPermissions()) {
+            wifi.startScan();
+        } else {
+            setPermissions();
+        }
+
     }
 
 
@@ -112,5 +125,70 @@ public class MainActivity extends AppCompatActivity implements OnListItemClick, 
         wifi.reconnect();
 
 
+    }
+
+    private boolean checkPermissions() {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return false;
+        }
+        return true;
+    }
+
+    private void m(){
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        MY_PERMISSIONS_REQUEST);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+    }
+    private void setPermissions() {
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSIONS_REQUEST);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode != MY_PERMISSIONS_REQUEST) {
+            return;
+        }
+        boolean isGranted = true;
+        for (int result : grantResults) {
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                isGranted = false;
+                break;
+            }
+        }
+
+        if (isGranted) {
+            startApplication();
+        } else {
+            Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show();
+
+        }
+    }
+    public void startApplication() {
+        results = wifi.getScanResults();
+        progressDialog.show();
+        wifi.startScan();
     }
 }
